fbf1c8c4a31912d4f0fb92a703e606718622cdc4
Grant Brown <BrownG@pragmatics.com>
2018-09-17 15:41:35 -0400

    restructured meeting objects to actually make elements as opposed to pure text

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -20,16 +20,29 @@ export class CalendarComponent implements OnInit {
     let calendarMap = meetings.map((meeting) => {
       let element = document.getElementById('meetings');
       let calendarCube = document.createElement('div');
+      calendarCube.classList.add('calendar-cube');
+
+      let dateBox = document.createElement('p');
+      let titleBox = document.createElement('p');
+      let employeeBox = document.createElement('ul');
+      
       let date = document.createTextNode(meeting.date);
       let title = document.createTextNode(meeting.title);
-      calendarCube.appendChild(date);
-      calendarCube.appendChild(title);
+      
+      dateBox.appendChild(date);
+      titleBox.appendChild(title);
+
+      calendarCube.appendChild(dateBox);
+      calendarCube.appendChild(titleBox);
+
       element.appendChild(calendarCube);
 
       for (let i = 0; i < meeting.employees.length; i++) {
         console.log(meeting.employees[i] + i);
+        let employeeNameBox = document.createElement('li');
         let employeeName = document.createTextNode(meeting.employees[i].name);
-        calendarCube.appendChild(employeeName);
+        employeeNameBox.appendChild(employeeName);
+        calendarCube.appendChild(employeeNameBox);
       }
 
     })
